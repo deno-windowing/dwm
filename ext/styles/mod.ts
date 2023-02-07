51503b7426aa72759013238f9e40a69d8fbff5c7
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-02-07 08:56:21 -0500

    feat(ext/styles): temp darwin & linux

--- ext/styles/mod.ts
+++ ext/styles/mod.ts
@@ -1,11 +1,19 @@
 import { StylePlatform } from "./platform.ts";
 import WIN from "./windows.ts";
+import DARWIN from "./darwin.ts";
+import LINUX from "./linux.ts";
 
 let platform: StylePlatform;
 
 switch (Deno.build.os) {
   case "windows":
     platform = WIN;
+    break;
+  case "darwin":
+    platform = DARWIN;
+    break;
+  case "linux":
+    platform = LINUX;
     break;
   default:
     throw new Error(`Unsupported platform: ${Deno.build.os}`);
